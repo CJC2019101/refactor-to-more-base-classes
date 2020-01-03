ab6e642543b13f699c2185573f6fd5e2357344fb
cjf <2196124754@qq.com>
2020-01-03 17:33:23 +0800

    first commit

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @author JianfeiChen
+ * @date 2020/1/3 17:31
+ */
+public class Animal {
+    String name;
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
---
+++ src/main/java/com/github/hcsp/inheritance/Person.java
@@ -0,0 +1,19 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @author JianfeiChen
+ * @date 2020/1/3 17:28
+ */
+public class Person {
+    String name;
+    int age;
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
